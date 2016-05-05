04e6742b6be6e1ebb1448f030a3a9857fe0ea3ec
Oukilian <mahieu66@gmail.com>
2016-05-05 21:11:53 +0200

    mise a jour du travail

---
+++ sujet_recent/profile.php
@@ -0,0 +1,3 @@
+<article><img src="css/image/avatar.png" /><br/>
+<i>Pseudo : Oukilian</i><br/>
+</article>
--- sujet_recent/sujet_recent.php
+++ sujet_recent/sujet_recent.php
@@ -1,7 +1,7 @@
+<p id="sujet_recent">Sujet Recent</p>
 <?php
     
 	$ver = $bdd->query('SELECT * FROM msg ORDER BY id DESC');
-	echo("<br />");
     $cpt = 0;
 	while($donnee = $ver->fetch()){
         if($cpt < 10){
